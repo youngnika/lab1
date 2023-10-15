3dffe286f70170567f506ca44186bc5cb71aaf9d
youngnika <113311967+youngnika@users.noreply.github.com>
2023-10-15 23:22:29 +0300

    Add files via upload

---
+++ Лабораторная 1/task1.py
@@ -0,0 +1,15 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+summa = 0
+index_of_none = 0
+for i in range(len(numbers)):
+    if numbers[i] is not None:
+        summa += numbers[i]
+    else:
+        index_of_none = i
+
+average_num = summa / len(numbers)
+numbers[index_of_none] = average_num
+
+print("Измененный список:", numbers)
